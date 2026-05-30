7161c0b009cf69ddbbc3cb892e922dde847a48cb
peterwuhua <peterwu.hua@qq.com>
2026-05-30 16:50:50 +0800

    初始化新分支

---
+++ SpringAISseServer/src/main/java/com/msb/SSEServerApp.java
@@ -0,0 +1,13 @@
+package com.msb;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSEServerApp
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
