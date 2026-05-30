0b01457082358f0b544ead27fdf8557654db7abd
peterwuhua <peterwu.hua@qq.com>
2026-05-30 15:29:55 +0800

    配置文件提交

---
+++ src/main/java/com/msb/SpringAIApp.java
@@ -0,0 +1,14 @@
+package com.msb;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAIApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringAIApp.class,args);
+    }
+}
